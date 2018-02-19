95cc947a85940332188a0c391355a82a0f08c879
ucd-os-chartreuse-s18 <matthew.moltzau@ucdenver.edu>
2018-02-19 12:29:46 -0700

    Minor changes (just testing git config --local to better handle multiple accounts

--- src/tests/threads/custom-test.c
+++ src/tests/threads/custom-test.c
@@ -4,7 +4,6 @@
 // headers copied over from test_alarm_negative, so
 // I do not know what they each are for.
 #include <stdio.h>
-//#include "/home/pintos/pintos/src/tests/threads/tests.h"
 #include "tests/threads/tests.h" /* location of extern */
 //#include "threads/malloc.h"
 //#include "threads/synch.h"
@@ -18,5 +17,5 @@ test_custom_test (void)
   printf("Inside the custom test.");
   //Looks like this indicates that the test passes
   pass();
-  //What if we do: fail();?
+  // the alternate is calling fail();
 }
