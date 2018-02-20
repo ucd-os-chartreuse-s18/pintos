a989b77d407cf8f8b9afdea021a7a22c540c9419
Pintos Developer <pintos@pintos.com>
2018-02-19 18:18:32 -0700

    Learned more about fail

--- src/tests/threads/custom-test.c
+++ src/tests/threads/custom-test.c
@@ -1,21 +1,14 @@
-/* Cutsom test  Only requirement is that it not crash.
- * TODO find a way to create another requirement */
+/* Cutsom test  No requirements */
 
-// headers copied over from test_alarm_negative, so
-// I do not know what they each are for.
 #include <stdio.h>
 #include "tests/threads/tests.h" /* location of extern */
-//#include "threads/malloc.h"
-//#include "threads/synch.h"
-//#include "threads/thread.h"
-//#include "devices/timer.h"
 
 //Created externally in tests.h
 void
 test_custom_test (void)
 {
   printf("Inside the custom test.");
-  //Looks like this indicates that the test passes
   pass();
-  // the alternate is calling fail();
+  //fail works like printf, but it also PANICS the kernel
+  //fail("Failing is possible even if you pass.");
 }
