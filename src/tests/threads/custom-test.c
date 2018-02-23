710aed3989b92703383f372d871ac6d97870c3d3
moltzaum <matthew@moltzau.net>
2018-02-22 19:00:11 -0700

    Revert "Learned more about fail"
    This reverts commit a989b77d407cf8f8b9afdea021a7a22c540c9419.
    
    revert #8

--- src/tests/threads/custom-test.c
+++ src/tests/threads/custom-test.c
@@ -1,14 +1,21 @@
-/* Cutsom test  No requirements */
+/* Cutsom test  Only requirement is that it not crash.
+ * TODO find a way to create another requirement */
 
+// headers copied over from test_alarm_negative, so
+// I do not know what they each are for.
 #include <stdio.h>
 #include "tests/threads/tests.h" /* location of extern */
+//#include "threads/malloc.h"
+//#include "threads/synch.h"
+//#include "threads/thread.h"
+//#include "devices/timer.h"
 
 //Created externally in tests.h
 void
 test_custom_test (void)
 {
   printf("Inside the custom test.");
+  //Looks like this indicates that the test passes
   pass();
-  //fail works like printf, but it also PANICS the kernel
-  //fail("Failing is possible even if you pass.");
+  // the alternate is calling fail();
 }
