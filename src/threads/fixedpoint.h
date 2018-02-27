8de04e4879d95de682549222e82eb75d18fd58cf
ucd-os-chartreuse-s18 <ghedricksce@gmail.com>
2018-02-27 10:10:46 -0700

    whoops

--- src/threads/fixedpoint.h
+++ src/threads/fixedpoint.h
@@ -22,9 +22,9 @@ typedef struct
 
 int fix_to_int_floor (fixed_point x);     // convert fixed to int, round toward zero
 int fix_to_int_round (fixed_point x);     // convert fixed to int, normal rounding
-fixed_point int_to_fixed (int n);         // convert integer to 17.14 fixed
-fixed_point add_fixed (fixed_point x, fixed_point y); // add fixed pt values
-fixed_point sub_fixed (fixed_point x, fixed_point y); // subtracted fix pt values
+fixed_point int_to_fix (int n);         // convert integer to 17.14 fixed
+fixed_point add_fix_fix (fixed_point x, fixed_point y); // add fixed pt values
+fixed_point sub_fix_fix (fixed_point x, fixed_point y); // subtracted fix pt values
 fixed_point add_int_fix (fixed_point x, int n); // add an integer to a fixed point
 fixed_point sub_int_fix_(fixed_point x, int n); // sub an integer from a fixed point
 fixed_point mul_fix_fix (fixed_point x, fixed_point y);// multiply two fixed points
