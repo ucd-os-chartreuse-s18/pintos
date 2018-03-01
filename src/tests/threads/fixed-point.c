e7c7e10fce88f06c8815fcfcc2220e77b57fdf06
ucd-chartreuse-s18 <matthew.moltzau@ucdenver.edu>
2018-02-28 21:51:54 -0700

    Forgot the actual test files

---
+++ src/tests/threads/fixed-point.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "tests/threads/tests.h" /* location of extern */
+#include "threads/fixed-point.h" 
+
+void
+test_fixed_point (void)
+{   
+  printf("FIXED POINT");
+  //fix_to_int_floor
+  //fix_to_int_round
+  //int_to_fix
+  //add_fix_fix
+  //add_int_fix
+  //sub_fix_fix
+  //sub_int_fix
+  //mul_fix_fix
+  //mul_int_fix
+  //div_fix_fix
+  //div_fix_int
+  
+  //fixed_point f = int_to_fix(10);
+  
+  //pass();
+  //fail();
+}   
+
---
+++ src/tests/threads/fixed-point.ck
@@ -0,0 +1,10 @@
+# -*- perl -*-
+use strict;
+use warnings;
+use tests::tests;
+check_expected ([<<'EOF']);
+(fixed-point) begin
+(fixed-point) Expected output line1
+(fixed-point) end
+EOF
+pass;
